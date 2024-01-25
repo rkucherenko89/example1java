299264cd28a811783bbca533b9376830c702922c
Roman Kucherenko <roxasmail911@gmail.com>
2024-01-24 21:22:06 -0500

    example11 \nusing @Autowire annotation on class field

--- src/main/java/com/example/config/ProjectConfig.java
+++ src/main/java/com/example/config/ProjectConfig.java
@@ -3,22 +3,10 @@ package com.example.config;
 import com.example.beans.Person;
 import com.example.beans.Vehicle;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "com.example.beans")
 public class ProjectConfig {
-	@Bean
-	public Vehicle vehicle() {
-		Vehicle vehicle = new Vehicle();
-		vehicle.setName("Toyota");
-		return vehicle;
-	}
-
-	@Bean
-	public Person person(Vehicle vehicle) {
-		Person person = new Person();
-		person.setName("Hanzo");
-		person.setVehicle(vehicle);
-		return person;
-	}
 }
