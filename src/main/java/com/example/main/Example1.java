14021898c1da9fe03ccf50e3cb8c7ac39dfafb20
Roman Kucherenko <roxasmail911@gmail.com>
2024-01-25 00:17:20 -0500

    deleted whole line

--- src/main/java/com/example/main/Example1.java
+++ src/main/java/com/example/main/Example1.java
@@ -11,7 +11,6 @@ public class Example1 {
 		Person person = context.getBean(Person.class);
 		Vehicle vehicle = context.getBean(Vehicle.class);
 
-		System.out.printf("Vehicle is %s%n", vehicle.getName());
 		System.out.printf("Person name is %s%n", person.getName());
 		System.out.printf("%s's vehicle name is %s%n", person.getName(), person.getVehicle().getName());
  	}
